0db160e56ab399c559d912e52c1936f0734b7fc2
Aliyu Haruna Khalifa <aliyoshk@hotmail.com>
2021-11-14 22:47:44 +0100

    Add project files.

---
+++ SOLExercice/Employee.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLExercice
+{
+    public abstract class Employee
+    {
+        private int _empID;
+        private string _loginName;
+        private string _password;
+        private string _department;
+        private string weekendTimeTable;
+        private string normalTimeTable;
+
+        public int EmployeeID
+        {
+            get { return _empID; }
+        }
+
+        public string LoginName
+        {
+            get { return _loginName; }
+            set { _loginName = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public string Department
+        {
+            get { return _department; }
+        }
+
+        public string WeekendTimeTable
+        {
+            get { return weekendTimeTable; }
+        }
+
+        public string NormalTimeTable
+        {
+            get { return normalTimeTable; }
+        }
+
+        public virtual Boolean Login(string loginName, string password)
+        {
+            return true;
+        }
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        private int _empID;
+        private string _loginName;
+        private string _password;
+        private string _department;
+        private string normalTimeTable;
+        public override Boolean Login(string loginName, string password)
+        {
+            LoginName = loginName;
+            Password = Password;
+
+            if (loginName == "aliyu" & password == "abc")
+            {
+                
+                return true; //Returning base Details
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    public class PartTimeEmployee : Employee
+    {
+        private int _empID;
+        private string _loginName;
+        private string _password;
+        private string _department;
+        private string weekendTimeTable;
+        public override Boolean Login(string loginName, string password)
+        {
+            LoginName = loginName;
+            Password = Password;
+
+            if (loginName == "khalifa" & password == "cba")
+            {
+                _empID = 201;
+                _department = "IT Center for Distance Learning";
+                return true; //Returning child Details
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+    }
+  
+}
---
+++ SOLExercice/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SOLExercice
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee employee;
+            string name, pass;
+
+            Console.WriteLine("Enter your Username and Password");
+            name = Console.ReadLine();
+            pass = Console.ReadLine();
+
+            employee = new FullTimeEmployee();
+            employee = new PartTimeEmployee();
+            employee.Login(name, pass);
+
+            Console.WriteLine("Employee Name "  +employee.LoginName);
+            Console.WriteLine("Employee Password " +employee.Password);
+            Console.WriteLine("Employee ID " + employee.EmployeeID);
+            Console.WriteLine("Employee Department " + employee.Department);
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
